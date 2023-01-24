7e8a3c1c854d54561264ef0fd0b22c4565d0b10a
Juan Manuel Rincón <jrinconnavarro.guadalupe@alumnado.fundacionloyola.net>
2023-01-24 20:33:35 +0100

    añado sql de inserción en categorias y profesores

---
+++ Scripts/inserción_masiva_profesores_categorias.sql
@@ -0,0 +1,39 @@
+/*Juan Manuel Rincón Navarro*/
+
+INSERT INTO CATEGORIAS(nombre)
+VALUES ('categoria1'),
+('categoria2'),
+('categoria3'),
+('categoria4'),
+('categoria5'),
+('categoria6'),
+('categoria7'),
+('categoria8'),
+('categoria9'),
+('categoria10'),
+('categoria11'),
+('categoria12'),
+('categoria13'),
+('categoria14'),
+('categoria15'),
+('categoria16'),
+('categoria17');
+
+INSERT INTO PROFESORES(nombre,correo,password)
+VALUES ('Jose Luis Canales','jlcanales@gmail.com','123456'),
+('María Ortiz','mortiz@gmail.com','123456'),
+('Mario Sánchez','msanchez@gmail.com','123456'),
+('Sergio Pérez','sperez@gmail.com','123456'),
+('Saúl Montes','smontes@gmail.com','123456'),
+('Juan Navarro','jnavarro@gmail.com','123456'),
+('Lucía Montero','lmontero@gmail.com','123456'),
+('Carlos Durán','cduran@gmail.com','123456'),
+('Francisco Torres','ftorres@gmail.com','123456'),
+('Ana Suizo','asuizo@gmail.com','123456'),
+('Pedro Jarque','pjarque@gmail.com','123456'),
+('Manuel Cano','mcano@gmail.com','123456'),
+('Sara Rincón','srincon@gmail.com','123456'),
+('David Soler','dsoler@gmail.com','123456'),
+('Isabel Rayo','srayo@gmail.com','123456'),
+('Oriol Casas','ocasas@gmail.com','123456'),
+('Iker Merino','imerino@gmail.com','123456');
